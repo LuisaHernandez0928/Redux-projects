7be82e87bb6b935c948fb4dd49c086f710968332
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-02-06 19:25:36 -0500

    Created Counter.js

--- src/app/store.js
+++ src/app/store.js
@@ -2,5 +2,5 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from '../features/counter/counterSlice/counterSlice'
 
 export const store = configureStore({ reducer: {
-    counter: counterReducer
+    counter: counterReducer,
 } });
