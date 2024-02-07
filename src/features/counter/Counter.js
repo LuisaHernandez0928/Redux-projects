5d340b19b8b08582fa36a4b276e7e1277f4357e6
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-02-06 19:45:56 -0500

    logic for incrementAmount state and dispatch

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const addValue = Number(incrementAmount) || 0;
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
+
   return (
     <section>
       <p>{count}</p>
